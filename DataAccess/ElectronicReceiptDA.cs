71ebdf316bf2f4447e4ebe58569f844969457ce3
Miguel Aspilcueta <adrian_30escorpio99@hotmail.com>
2024-01-31 10:16:19 -0500

    FE-120 Se guardaron mas datos de facturacion

--- DataAccess/ElectronicReceiptDA.cs
+++ DataAccess/ElectronicReceiptDA.cs
@@ -21,34 +21,32 @@ namespace DataAccess
 
                 var serieCorrelativo = documento.IdDocumento.Split('-');
 
+                db.AddInParameter(cmd, "project", DbType.String, documento.Project);
+                db.AddInParameter(cmd, "format", DbType.String, documento.formato);
+                db.AddInParameter(cmd, "senderDocumentTypeId", DbType.Int32, int.Parse(documento.Emisor.TipoDocumento));
+                db.AddInParameter(cmd, "senderDocument", DbType.String, documento.Emisor.NroDocumento);
+                db.AddInParameter(cmd, "senderName", DbType.String, documento.Emisor.NombreLegal);
                 db.AddInParameter(cmd, "series", DbType.String, serieCorrelativo[0]);
                 db.AddInParameter(cmd, "correlative", DbType.Int32, int.Parse(serieCorrelativo[1]));
                 db.AddInParameter(cmd, "receiptTypeId", DbType.Int32, int.Parse(documento.TipoDocumento));
-                db.AddInParameter(cmd, "documentTypeReceiptId", DbType.Int32, int.Parse(documento.Receptor.TipoDocumento));
-                db.AddInParameter(cmd, "document", DbType.String, documento.Receptor.NroDocumento);
-                db.AddInParameter(cmd, "name", DbType.String, documento.Receptor.NombreLegal);
+                db.AddInParameter(cmd, "recipientDocumentTypeId", DbType.Int32, int.Parse(documento.Receptor.TipoDocumento));
+                db.AddInParameter(cmd, "recipientDocument", DbType.String, documento.Receptor.NroDocumento);
+                db.AddInParameter(cmd, "recipientName", DbType.String, documento.Receptor.NombreLegal);
+                db.AddInParameter(cmd, "discount", DbType.Decimal, documento.DescuentoGlobal);
                 db.AddInParameter(cmd, "subtotal", DbType.Decimal, documento.Gravadas);
                 db.AddInParameter(cmd, "totalIGV", DbType.Decimal, documento.TotalIgv);
                 db.AddInParameter(cmd, "total", DbType.Decimal, documento.TotalVenta);
                 db.AddInParameter(cmd, "acceptedBySunat", DbType.Boolean, pEnviarDocumentoResponse.Exito);
                 db.AddInParameter(cmd, "sunatDescription", DbType.String, pEnviarDocumentoResponse.MensajeRespuesta);
-                db.AddInParameter(cmd, "canceled", DbType.Boolean, false);
                 db.AddInParameter(cmd, "qrCode", DbType.String, pEnviarDocumentoResponse.qrCode);
                 db.AddInParameter(cmd, "pdfLink", DbType.String, pEnviarDocumentoResponse.pdfPath);
                 db.AddInParameter(cmd, "xmlLink", DbType.String, pEnviarDocumentoResponse.xmlPath);
                 db.AddInParameter(cmd, "issueDate", DbType.DateTime, documento.FechaEmision);
+                db.AddInParameter(cmd, "issueTime", DbType.String, documento.HoraEmision);
                 db.AddInParameter(cmd, "currency", DbType.String, documento.Moneda);
-                db.AddInParameter(cmd, "userCreated", DbType.String, documento.UserCreated);
-
-                /*
-                //DATOS DE EMPRESA EMISORA Y PROYECTO
-                db.AddInParameter(cmd, "document", DbType.String, documento.Emisor.NombreLegal);
-                db.AddInParameter(cmd, "document", DbType.String, documento.Emisor.TipoDocumento);
-                db.AddInParameter(cmd, "document", DbType.String, documento.Emisor.NroDocumento);
-
-                // CODIGO ERROR Y MENSAJE ERROR
                 db.AddInParameter(cmd, "errorMessage", DbType.String, pEnviarDocumentoResponse.MensajeError);
-                */
+                db.AddInParameter(cmd, "cdrTicketNumber", DbType.String, pEnviarDocumentoResponse.NroTicketCdr);
+                db.AddInParameter(cmd, "userCreated", DbType.String, documento.UserCreated);
 
                 db.ExecuteNonQuery(cmd);
             }
